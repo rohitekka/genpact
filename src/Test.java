5da24a39f7dfb22e887b703dbbd89379f4c0663a
Ekka <703247434@genpact.com>
2019-09-03 12:57:40 +0530

    version 1st

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		long no_of_items=45;
+		float price_per_unit=546.34f;
+		double total_cost=no_of_items*price_per_unit;
+		System.out.println("total price= "+total_cost);
+		
+		int a=2,b=3,c=4,d=5,e=7;
+		float avg=(a+b+c+d+e)/5.0f;
+		System.out.println("average = "+avg);
+		
+		
+	}
+
+}
